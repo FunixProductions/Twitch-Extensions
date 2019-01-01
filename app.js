5103497ab03623f33dca1a5ce345e92e2d997403
Antoine <antoine.pronnier@epitech.eu>
2019-01-01 16:34:11 +0100

    adding front need to put png

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ xhr.onreadystatechange = function(channel) {
     if (xhr.readyState == 4) {
         var data = JSON.parse(xhr.responseText)
         var elm  = document.getElementById("info")
+        var icon = document.getElementById("js-status-icon")
+        var text = document.getElementById("js-status-text")
         if (data["data"].length === 0) {
-            elm.style.color = "red"
-            elm.innerHTML = "FunixGaming n'est pas en live actuellement :("
+            elm.style.color = "#f7f1e3"
+            icon.style.backgroundColor = "#b33939"
+            text.style.backgroundColor = "#ff5252"
+            elm.innerHTML = "FunixGaming n'est pas en live actuellement."
         } else {
-            elm.style.color = "green"
+            elm.style.color = "black"
+            icon.style.backgroundColor = "#20bf6b"
+            text.style.backgroundColor = "#26de81"
             elm.innerHTML = "Viens voir FunixGaming en live maintenant !"
         }
     }
